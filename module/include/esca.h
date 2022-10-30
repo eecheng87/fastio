17b92ee3f77bbeeb25b11f95fa66d97c630a4d6d
eecheng87 <yucheng871011@gmail.com>
2022-10-30 13:09:02 +0000

    Cleanup

--- module/include/esca.h
+++ module/include/esca.h
@@ -8,7 +8,7 @@
 
 #include <stdatomic.h>
 
-#define DEFAULT_CONFIG_PATH "/home/eecheng/SAIO/esca.conf"
+#define DEFAULT_CONFIG_PATH "/home/eecheng/fastio/esca.conf"
 #define CONFIG_ARG_MAX_BYTES 128
 
 /* Limit */
